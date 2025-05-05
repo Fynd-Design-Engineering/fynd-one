c590715151ce05f6b2059c6e96c569c8494fcdc3
Raimon <raimonrts@gmail.com>
2025-05-05 20:59:33 +0530

    added announcement slider

---
+++ navigation/announcement.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const announcementSwiper = new Swiper(".announcement-slider", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+    loop: true,
+    speed: 800,
+    pagination: {
+      el: ".announcement-pagination",
+      clickable: true,
+    },
+    on: {
+      init: function () {
+        if (this.slides.length <= 1) {
+          const paginationEl = document.querySelector(
+            ".announcement-pagination"
+          );
+          if (paginationEl) {
+            paginationEl.style.display = "none";
+          }
+          this.autoplay.stop();
+        }
+      },
+    },
+  });
+});
